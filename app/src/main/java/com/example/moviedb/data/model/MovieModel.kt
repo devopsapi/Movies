433308e3fb5dd95a03755e960c04c39d40434487
ylevytska <levitskaya404@gmail.com>
2022-01-22 23:43:40 +0200

    Add different recyclerView layouts for different orientation

--- app/src/main/java/com/example/moviedb/data/model/MovieModel.kt
+++ app/src/main/java/com/example/moviedb/data/model/MovieModel.kt
@@ -1,3 +1,30 @@
 package com.example.moviedb.data.model
 
-data class MovieModel( val id: Int, val poster_path: String)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class MovieModel( val id: Int, val poster_path: String): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString().toString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(poster_path)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MovieModel> {
+        override fun createFromParcel(parcel: Parcel): MovieModel {
+            return MovieModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MovieModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
