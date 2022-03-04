3b9e9f8053c4955467045dd2853c5c1c7c82f563
devopsapi <levitskaya404@gmail.com>
2022-03-04 23:06:47 +0200

    Replace manual switching tabs with ViewPager2

---
+++ app/src/main/java/com/example/moviedb/ui/adapters/TabMoviesAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.moviedb.ui.adapters
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.moviedb.ui.screens.home.TabMovieFragment
+
+class TabMoviesAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+    private val tabsAmount = 5
+    private val positionKey = "Current position"
+
+    override fun getItemCount(): Int = tabsAmount
+
+    override fun createFragment(position: Int): Fragment {
+        val bundle = Bundle()
+        bundle.putInt(positionKey, position)
+
+        val fragment = TabMovieFragment()
+        fragment.arguments = bundle
+
+        return fragment
+    }
+}
--- app/src/main/java/com/example/moviedb/ui/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/moviedb/ui/screens/home/HomeFragment.kt
@@ -1,29 +1,20 @@
 package com.example.moviedb.ui.screens.home
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
-import androidx.fragment.app.FragmentTransaction
-import com.example.moviedb.R
 import com.example.moviedb.databinding.FragmentHomeBinding
-import com.example.moviedb.ui.screens.home.search.SearchFragment
-import com.google.android.material.tabs.TabLayout
+import com.example.moviedb.ui.adapters.TabMoviesAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
-    private lateinit var fragmentTransaction: FragmentTransaction
-    private lateinit var fragment: Fragment
-
-    private var currentTabPosition = 0
-    private val positionKey = "Current position"
-    private val queryKey = "Query"
+    private lateinit var tabsAdapter: TabMoviesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,89 +27,22 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if (savedInstanceState != null) {
-            currentTabPosition = savedInstanceState.getInt(positionKey)
-        }
+        tabsAdapter = TabMoviesAdapter(this)
+        binding.pager.adapter = tabsAdapter
 
-        binding.tabsLayout.selectTab(binding.tabsLayout.getTabAt(currentTabPosition))
-
-        val bundle = Bundle()
-        bundle.putInt(positionKey, currentTabPosition)
-
-        fragment = TabMovieFragment()
-        fragment.arguments = bundle
-        fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, fragment)
-            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-            .commit()
-
-        binding.tabsLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                // Handle tab select
-
-                currentTabPosition = tab?.position ?: 0
-
-                bundle.putInt(positionKey, currentTabPosition)
-
-                fragment = TabMovieFragment()
-                fragment.arguments = bundle
-                fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
-                fragmentTransaction.replace(R.id.frameLayout, fragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-
-                binding.tabsLayout.tabSelectedIndicator.alpha = 255
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                // Handle tab reselect
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                // Handle tab unselected
-            }
-        })
-
-        setUpSearch()
+        TabLayoutMediator(binding.tabsLayout, binding.pager) { tab, position ->
+            tab.text = defineTabName(position)
+        }.attach()
     }
 
-    private fun setUpSearch() {
-        binding.searchView.apply {
-            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                @SuppressLint("ResourceAsColor")
-                override fun onQueryTextSubmit(query: String?): Boolean {
-                    if (query != null) {
-                        clearFocus()
-
-                        binding.tabsLayout.selectTab(null)
-                        binding.tabsLayout.tabSelectedIndicator.alpha = 0
-
-                        val bundle = Bundle()
-                        bundle.putString(queryKey, query)
-
-                        fragment = SearchFragment()
-                        fragment.arguments = bundle
-
-                        fragmentTransaction =
-                            requireActivity().supportFragmentManager.beginTransaction()
-                        fragmentTransaction.replace(R.id.frameLayout, fragment)
-                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                            .addToBackStack(null)
-                            .commit()
-                    }
-                    return false
-                }
-
-                override fun onQueryTextChange(newText: String?): Boolean {
-                    return false
-                }
-            })
+    private fun defineTabName(position: Int): String {
+        return when (position) {
+            0 -> "Popular"
+            1 -> "Latest"
+            2 -> "Now playing"
+            3 -> "Top rated"
+            4 -> "Upcoming"
+            else -> "Popular"
         }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(positionKey, currentTabPosition)
     }
 }
